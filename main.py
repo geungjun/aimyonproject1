6d37452639487b4bc86054611697cf6af7881a1c
geungjun <l01047302368@gmail.com>
2025-08-05 11:25:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.title('야르 처음 만드는 스트림릿')
 st.write('안녕 반가워')
 st.write('야르야르')
-st.dataframe('인터랙티브한가요?')
+data = pd.DataFrame({
+    '숫자': [1, 2, 3, 4],
+    '제곱': [1, 4, 9, 16]
+})
