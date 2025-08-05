5975d6c952d392c8879d7e0cccba8a39bc252e62
geungjun <l01047302368@gmail.com>
2025-08-05 11:28:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import streamlit as st
 import pandas as pd
 
+#제목 쓰는 것
 st.title('야르 처음 만드는 스트림릿')
+#내용 쓰는 것
 st.write('안녕 반가워')
 st.write('야르야르')
+
+#표
 data = pd.DataFrame({
     '숫자': [1, 2, 3, 4],
     '제곱': [1, 4, 9, 16]
 })
 st.dataframe(data)
+
+#입력받기
+st.text_input('이름이나 써보거라')
